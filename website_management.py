68b5b8535a0cc7ce5090552669ac6793f1d8ed54
Mohamed Oussaren <oussarenmohamed@gmail.com>
2023-03-02 13:41:39 +0100

    Change dowload button position

--- website_management.py
+++ website_management.py
@@ -240,7 +240,7 @@ def display_search_bar(select_data: str = "datasets") -> tuple:
         label_search = ".gro files quick search"
     elif select_data == "mdp":
         label_search = ".mdp files quick search"
-    col_keyup, col_filter, col_download, _ = st.columns([4, 2, 2, 1])
+    col_keyup, col_filter, col_download, _ = st.columns([4, 1.2, 1, 1])
     with col_keyup:
         search = st_keyup(label_search, placeholder=placeholder)
     return search, col_filter, col_download
